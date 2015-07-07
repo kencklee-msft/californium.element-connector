a344771ce259d9ebddbe5a675b53bff373eb2bae
LDaPrato <LDaPrato@zebra.com>
2015-07-07 12:07:23 -0500

    Removed StatefulConnector

--- src/main/java/org/eclipse/californium/elements/StatefulConnector.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.californium.elements;
-
-import org.eclipse.californium.elements.tcp.ConnectionStateListener;
-
-public interface StatefulConnector extends Connector{
-
-	public void addConnectionStateListener(ConnectionStateListener listener);
-}
